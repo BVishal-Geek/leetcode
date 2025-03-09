936c920b63f750a028a3b343b8c4a58793edcbe2
Vishal Bakshi <82230361+BVishal-Geek@users.noreply.github.com>
2025-03-09 17:36:48 -0400

    Time: 790 ms (18.54%), Space: 21.4 MB (57.95%) - LeetHub

---
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+
+    def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
+        l = 0
+        res = 0
+        N = len(colors)
+        for r in range(1, N+k-1):
+            if colors[r%N] == colors[(r-1)%N]:
+                l = r
+
+            if r-l+1>k:
+                l += 1    
+
+            if r-l+1 == k:
+                res+=1
+            
+            
+
+
+        return res
