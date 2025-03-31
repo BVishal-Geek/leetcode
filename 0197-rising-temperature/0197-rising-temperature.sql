45e942ff56dc980d2597256f53e49023332121fc
Vishal Bakshi <82230361+BVishal-Geek@users.noreply.github.com>
2025-03-30 22:08:25 -0400

    Time: 500 ms (89.12%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,12 +1,20 @@
-WITH cte AS (
+-- WITH cte AS (
 
-SELECT  id,
-        recordDate,
-        temperature,
-        DATEDIFF(recordDate, LAG(recordDate) OVER(ORDER BY recordDate)) AS DateDifference,
-        temperature - LAG(temperature) OVER(ORDER BY recordDate) AS Diff
-    FROM Weather
-)
+-- SELECT  id,
+--         recordDate,
+--         temperature,
+--         DATEDIFF(recordDate, LAG(recordDate) OVER(ORDER BY recordDate)) AS DateDifference,
+--         temperature - LAG(temperature) OVER(ORDER BY recordDate) AS Diff
+--     FROM Weather
+-- )
 
-SELECT id FROM cte
-WHERE diff > 0 AND DateDifference = 1;
+-- SELECT id FROM cte
+-- WHERE diff > 0 AND DateDifference = 1;
+
+
+-- SELF JOIN
+SELECT W1.id
+FROM Weather W1
+JOIN Weather W2
+ON DATEDIFF(W1.recordDate, W2.recordDate) = 1
+WHERE w1.temperature > W2.temperature;
