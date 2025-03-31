d77bf8e2efd476b74ac78cd4216dc50a63065277
Vishal Bakshi <82230361+BVishal-Geek@users.noreply.github.com>
2025-03-30 20:34:05 -0400

    Time: 3 ms (91.26%), Space: 17.7 MB (57.56%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.py
+++ 0768-partition-labels/0768-partition-labels.py
@@ -1,19 +1,32 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        last_occurrence = {}
-        
-        for i, char in enumerate(s):
-            last_occurrence[char] = i
-        
-        result = []
-        start = 0
-        end = 0
-        
-        for i, char in enumerate(s):
-            end = max(end, last_occurrence[char])
-            
+        '''
+        Explanation: 
+            We have a string from which we need to partition them
+            in a way the each character should be only in one group. 
+            It can be repeated in the same group but it should not be
+            in the other group. 
+            We need to return a list where the list contains the length of
+            each string length. 
+
+        Logic:
+            We are checking the last occurance of each character. 
+            if the current character last occurance is greater than 
+            the last occurance of last character we will change the value. 
+            if the last occurance and the index of the current character equals same,
+            we get 1st partition of string. 
+        '''
+        lastIndex = {}
+
+        for i, c in enumerate(s):
+            lastIndex[c] = i
+
+        res = []
+        size, end = 0,0
+        for i, c in enumerate(s):
+            size +=1
+            end = max(end, lastIndex[c])
             if i == end:
-                result.append(end - start + 1)
-                start = i + 1
-        
-        return result
+                res.append(size)
+                size = 0
+        return res
