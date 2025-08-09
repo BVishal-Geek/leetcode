c72f51d2a7863051ed7d996e54773279eaf1a68b
Vishal Bakshi <82230361+BVishal-Geek@users.noreply.github.com>
2025-08-09 11:44:16 -0400

    Time: 31 ms (28.56%), Space: 37.4 MB (65.98%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        tempStorage1 = nums[0]
+        tempStorage2 = float('inf')
+
+        for i in range(1,len(nums)): 
+            if nums[i] <= tempStorage1: 
+                tempStorage1 = nums[i]
+            elif nums[i] <= tempStorage2:
+                tempStorage2 = nums[i]
+            else: 
+                return True
+        return False
+
+       
+            
