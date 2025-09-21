4010f1cbe3a62f3982eb4d550a753669dd4d5938
Vishal Bakshi <82230361+BVishal-Geek@users.noreply.github.com>
2025-09-21 18:35:58 -0400

    Time: 3 ms (39.07%), Space: 17.8 MB (48.55%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,43 +1,33 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        if not s:
-            return 0
-        
-        # Constants for 32-bit signed integer range
-        INT_MAX = 2**31 - 1
         INT_MIN = -2**31
+        INT_MAX = 2**31 - 1
+
         
-        i = 0
-        n = len(s)
-        
-        # Step 1: Skip leading whitespace
-        while i < n and s[i] == ' ':
-            i += 1
-        
-        # Check if we've reached the end
-        if i == n:
+        s = s.lstrip() 
+        if not s: 
             return 0
+
         
-        # Step 2: Check for sign
-        sign = 1
-        if s[i] == '+':
-            i += 1
-        elif s[i] == '-':
-            sign = -1
-            i += 1
-        
-        # Step 3: Read digits and convert
-        res = 0
-        while i < n and s[i].isdigit():
-            digit = int(s[i])
-            res = res * 10 + digit
+        j = 0
+        sign = 1 
+    
+        if s[j] == '+':
+            j += 1 
             
-            if sign * res <= INT_MIN:
-                return INT_MIN
-            if sign * res >= INT_MAX:
-                return INT_MAX
-            
-            i += 1
-        
-        # Step 4: Apply sign and return
-        return res * sign
+        elif s[j] == '-':
+            j += 1
+            sign = -1 
+
+        res = 0 
+        for i in range(j, len(s)):
+            if s[i].isdigit():
+                res = res * 10 + int(s[i])
+            else:
+                break
+       
+        if sign * res < INT_MIN:
+            return INT_MIN
+        if sign * res > INT_MAX:
+            return INT_MAX
+        return sign * res
