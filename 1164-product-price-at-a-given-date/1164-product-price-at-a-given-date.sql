94870c7d98b4522cfad70dae724f510fd5de9f04
Vishal Bakshi <82230361+BVishal-Geek@users.noreply.github.com>
2026-01-13 22:39:37 -0500

    Time: 705 ms (17.61%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,15 +1,28 @@
-with filter_date as (
-    select *
-    from Products
-    where change_date <= "2019-08-16"
-), 
-rank_prices as (
-    SELECT *,
-           RANK() OVER (PARTITION BY product_id ORDER BY change_date DESC) AS rnk
-    from filter_date
-)
+-- with filter_date as (
+--     select *
+--     from Products
+--     where change_date <= "2019-08-16"
+-- ), 
+-- rank_prices as (
+--     SELECT *,
+--            RANK() OVER (PARTITION BY product_id ORDER BY change_date DESC) AS rnk
+--     from filter_date
+-- )
 
-select uniq_ids.product_id, Coalesce(rp.new_price, 10) as price
-from (select distinct product_id from Products) as uniq_ids
-Left join rank_prices rp 
-On uniq_ids.product_id = rp.product_id and rp.rnk=1;
+-- select uniq_ids.product_id, Coalesce(rp.new_price, 10) as price
+-- from (select distinct product_id from Products) as uniq_ids
+-- Left join rank_prices rp 
+-- On uniq_ids.product_id = rp.product_id and rp.rnk=1;
+
+
+select dist_prods.product_id, coalesce(p.new_price,10) as price
+from (select distinct product_id from Products) as dist_prods
+left join Products p
+On p.product_id = dist_prods.product_id 
+and p.change_date = (
+    Select Max(change_date)
+    from Products
+    where product_id = dist_prods.product_id
+    and change_date <= "2019-08-16"
+)
+order by dist_prods.product_id asc; 
