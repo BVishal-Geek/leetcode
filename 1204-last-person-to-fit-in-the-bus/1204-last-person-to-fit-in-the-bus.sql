b590618a76863512fd9effef7c03dfed731f6962
Vishal Bakshi <82230361+BVishal-Geek@users.noreply.github.com>
2026-01-14 23:19:09 -0500

    Time: 2107 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,7 +1,17 @@
-select person_name from (
-    select person_name, Sum(weight) Over(Order by turn asc) as total_weight
-    from Queue
-    ) as E
-    where total_weight <= 1000
-    order by total_weight desc
-    limit 1;
+-- select person_name from (
+--     select person_name, Sum(weight) Over(Order by turn asc) as total_weight
+--     from Queue
+--     ) as E
+--     where total_weight <= 1000
+--     order by total_weight desc
+--     limit 1;
+
+
+select person_name from
+(
+    select person_name, turn, (select sum(weight) from Queue q2 where q2.turn <= q1.turn ) as total_weight
+    from Queue q1
+) as E
+where total_weight<=1000
+order by total_weight desc 
+limit 1;
