8d7d5f6958ec1012b38aacd01793a0a0865acda6
Vishal Bakshi <82230361+BVishal-Geek@users.noreply.github.com>
2026-03-09 18:36:44 -0400

    Time: 63 ms (25.97%), Space: 29.7 MB (33.31%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -3,32 +3,13 @@ class Solution:
     def area(l,b):
         return l*b
     def maxArea(self, height: List[int]) -> int:
-        # highest_num = 0 
-        # largest_area = 0
-        # index = 0 
-        # for i in range(len(height)):
-
-        #     if highest_num<height[i]:
-        #         highest_num = height[i]
-        #         index = i
-        
-        
-        # for j in range(len(height)): 
-
-        #     if highest_num == height[j]:
-        #         largest_area = max(largest_area, self.area(highest_num, abs(index - j)))
-            
-        #     else: 
-        #         largest_area = max(largest_area, self.area(height[j], abs(index - j)))
-
-        # return largest_area
-        left = 0
-        right = len(height) - 1 
-        largest_area = 0
-        while left<=right: 
-            largest_area = max(largest_area, min(height[left],height[right])*abs(right-left))
-            if height[left]<height[right] or height[left] == height[right]:
-                left += 1
+        left = 0 
+        right = len(height)-1
+        max_area = 0
+        while left<right:
+            max_area = max(max_area, self.area(min(height[left], height[right]), right-left))
+            if height[left]<height[right]:
+                left+=1
             else:
-                right -= 1
-        return largest_area
+                right-=1 
+        return max_area
