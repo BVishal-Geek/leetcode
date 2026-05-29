adaf7c2b329527f8d639008a191528671351cee2
Vishal Bakshi <82230361+BVishal-Geek@users.noreply.github.com>
2026-05-28 23:50:46 -0400

    Time: 43 ms (5.09%), Space: 20.7 MB (21.73%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,14 +2,15 @@ import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
 
-        reString = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+        reString = re.sub(r'[^a-zA-Z0-9]','',s).lower()
+
         left = 0
-        right = len(reString) - 1
-        
-        while left<=right: 
-            if reString[left] != reString[right]: 
+        right = len(reString) - 1 
+
+        while left<right: 
+            if reString[left]!=reString[right]:
                 return False
-            else: 
+            else:
                 left+=1
                 right-=1
         return True
